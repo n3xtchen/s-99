205e9668dab8e3299537e6ddd3e13408f6ea1908
n3xtchen <echenwen@gmail.com>
2017-03-11 23:38:13 +0800

    P08：去重

---
+++ src/main/scala/nextchen/P08.scala
@@ -0,0 +1,15 @@
+/*
+ * P08.scala
+ * Copyright (C) 2017 n3xtchen <echenwen@gmail.com>
+ *
+ * Distributed under terms of the GPL-2.0 license.
+ */
+
+object P08 {
+  // scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  // res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
+
+  def main(args: Array[String]) {
+  }
+}
+
