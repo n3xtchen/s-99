7318d487dcb24ea0df2fca182482be88ddbdcc83
n3xtchen <echenwen@gmail.com>
2017-03-26 16:05:58 +0800

    P10 P13:审题问题

--- src/main/scala/nextchen/P10.scala
+++ src/main/scala/nextchen/P10.scala
@@ -11,15 +11,7 @@ object P10 {
   // scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 	// res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
 
-  def encode[A](ls: List[A]) = {
-    def proc(list: List[(Int,A)]): List[(Int, A)] = list match {
-      case Nil => Nil
-      case head :: sec :: tail if sec._2 == head._2 =>
-        proc((head._1+sec._1, head._2) :: proc(tail))
-      case head :: tail => head :: proc(tail)
-    }
-    proc(ls.map((1, _)))
-  }
+  def encode[A](ls: List[A]) = P09.pack(ls).map(row => (row.length, row(0)))
 
   def main(args: Array[String]) {
     println(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
