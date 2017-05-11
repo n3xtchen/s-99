a2da80de3ce491b74944279bb29d2181f0983acc
n3xtchen <echenwen@gmail.com>
2017-05-11 10:09:38 +0800

    P50 实现 60%

--- src/main/scala/nextchen/P50.scala
+++ src/main/scala/nextchen/P50.scala
@@ -12,14 +12,23 @@ object P50 {
 	// res0: List[String, String] = List((a,0), (b,101), (c,100), (d,111), (e,1101), (f,1100))
   
   def huffman(l: List[(String, Int)]) = {
-    var tmp: List[(Any, Int)] = l
-    val keys = l.map(x => (x._1, "")).toMap
-    while (tmp.length > 1) {
+    import scala.collection.mutable.Map
+
+    def proc(
+      tmp: List[(Any, Int)],
+      result: Map[String, String] = Map.empty
+    ): Map[String, String] = if (tmp.length == 1) {
+      result 
+    } else {
       val ( a :: b :: tail) = tmp.sortBy(_._2)
-      tmp = ((a._1, b._1), a._2+b._2) :: tail
+      println(a, b)
+      result(a._1.toString) = "0" + result.getOrElse(a._1.toString, "")
+      result(b._1.toString) = "0" + result.getOrElse(b._1.toString, "")
+      proc((List(a._1, b._1), a._2+b._2) :: tail, result)
     }
-    tmp(0)._1
+    proc(l)
   }
+
 
   def main(args: Array[String]) {
     println(huffman(List(("a", 45), ("b", 13), ("c", 12), ("d", 16), ("e", 9), ("f", 5))))
