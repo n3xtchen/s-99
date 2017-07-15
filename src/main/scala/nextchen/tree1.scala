b9c95060e9c05ccbb66407e9ecb1e12901c5e104
n3xtchen <echenwen@gmail.com>
2017-07-15 12:24:14 +0800

    二叉树构建

---
+++ src/main/scala/nextchen/tree1.scala
@@ -0,0 +1,27 @@
+/*
+ * tree1.scala
+ * Copyright (C) 2017 n3xtchen <echenwen@gmail.com>
+ *
+ * Distributed under terms of the GPL-2.0 license.
+ */
+
+package nextchen
+
+sealed abstract class Tree[+T]
+
+case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
+  override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+}
+
+case object End extends Tree[Nothing] {
+  override def toString = "."
+}
+
+object Node {
+  def apply[T](value: T): Node[T] = Node(value, End, End)
+
+  def main(args: Array[String]) {
+    println(Node('a', Node('b', Node('d'), Node('e')), Node('c', End, Node('f', Node('g'), End))))
+  }
+}
+
